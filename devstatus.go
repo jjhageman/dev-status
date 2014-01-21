032ce4bcba0fd5acda9b5603d1262bb3d61f1b4a
Jeremy Hageman <jjhageman@gmail.com>
2014-01-21 01:02:25 -0800

    add init

--- devstatus.go
+++ devstatus.go
@@ -7,12 +7,19 @@ import (
 	"net/url"
 )
 
-func main() {
-	mux := tigertonic.NewTrieServeMux()
+var (
+	mux *tigertonic.TrieServeMux
+)
+
+func init() {
+	mux = tigertonic.NewTrieServeMux()
 	mux.Handle("GET", "/user/{id}", tigertonic.Timed(tigertonic.Marshaled(getUser), "getUser", nil))
+}
+
+func main() {
 	tigertonic.NewServer(":8000", tigertonic.Logged(mux, nil)).ListenAndServe()
 }
 
 func getUser(u *url.URL, h http.Header, rq *UserRequest) (int, http.Header, *UserResponse, error) {
-	return http.StatusOK, nil, &UserResponse{}, nil
+	return http.StatusOK, nil, &UserResponse{"Test", "User", "1234", "Available"}, nil
 }
--- devstatus_test.go
+++ devstatus_test.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	//"fmt"
 	"github.com/rcrowley/go-tigertonic/mocking"
-	//"net/http"
+	"net/http"
 	"testing"
 )
 
 func TestGetUser(t *testing.T) {
 	code, _, response, err := getUser(
-		mocking.URL(mux, "GET", ""),
+		mocking.URL(mux, "GET", "/user/ID"),
 		mocking.Header(nil),
-		nil)
+		nil,
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if code != http.StatusOK {
+		t.Fatal(code)
+	}
+	if "Bob" != response.FirstName {
+		t.Errorf("expected Bob, got %v", response.FirstName)
+		t.Fatal(response)
+	}
 }
